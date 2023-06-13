ac770042315c2099def1b8fa756f6e276e557b68
diepedyt <106111219+diepedyt@users.noreply.github.com>
2023-06-13 16:13:04 +0100

    v2

--- SimpleUiLib.lua
+++ SimpleUiLib.lua
@@ -16,7 +16,20 @@ function protectGui(GUI)
 end
 
 
-ui.CreateKeySystem = function(key, discord)
+ui.CreateKeySystem = function(key, discord, theme, title)
+	
+	if not theme then
+		theme = {
+			["MainColor"] = Color3.fromRGB(255, 255, 255),
+			["KeyUIColor"] = Color3.fromRGB(149, 149, 149),
+			["KeyUIBorderColor"] = Color3.fromRGB(27, 42, 53),
+			["KeyLoginTextBoxColor"] = Color3.fromRGB(255, 255, 255),
+			["KeyLoginTextColor"] = Color3.fromRGB(0, 0, 0),
+			["TextButtonColor"] = Color3.fromRGB(170, 170, 170),
+			["TextButtonTextColor"] = Color3.fromRGB(0, 0, 0)
+		}
+	end
+	
 	local KeyLogin = Instance.new("ScreenGui")
 	local Main = Instance.new("Frame")
 	local ImageUiCorner = Instance.new("UICorner")
@@ -31,26 +44,26 @@ ui.CreateKeySystem = function(key, discord)
 	Main.Name = "Main"
 	Main.Parent = KeyLogin
 	Main.AnchorPoint = Vector2.new(1, 0)
-	Main.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+	Main.BackgroundColor3 = theme["MainColor"]
 	Main.Position = UDim2.new(0.637653112, 0, 0.203301102, 0)
 	Main.Size = UDim2.new(0.276595742, 0, 0.59334296, 0)
 
 	FrameUICorner.CornerRadius = UDim.new(0, 5)
 	FrameUICorner.Name = "FrameUICorner"
 	FrameUICorner.Parent = Main
-	
+
 
 	local originalMainSize = Main.Size
-	
+
 	Main.Size = UDim2.new(0.277,0,0,0)
 	Main.BackgroundTransparency = 1
 	FrameUICorner.CornerRadius = UDim.new(0,5)
 
 	KeyLogin.Enabled = true
-	
+
 	ts:Create(Main, TweenInfo.new(1, Enum.EasingStyle.Quint), {Size = originalMainSize, BackgroundTransparency = 0}):Play()
 	task.wait(1)
-	
+
 	local KeyUI = Instance.new("Frame")
 	local TextBox = Instance.new("TextBox")
 	local UITextSizeConstraint = Instance.new("UITextSizeConstraint")
@@ -62,20 +75,20 @@ ui.CreateKeySystem = function(key, discord)
 	KeyUI.Name = "KeyUI"
 	KeyUI.Parent = Main
 	KeyUI.AnchorPoint = Vector2.new(0.5, 0)
-	KeyUI.BackgroundColor3 = Color3.fromRGB(149, 149, 149)
+	KeyUI.BackgroundColor3 = theme["KeyUIColor"]
 	KeyUI.Position = UDim2.new(0.5, 0, 0.0250000004, 0)
 	KeyUI.Size = UDim2.new(0, 0, 0.95, 0)
 
 	TextBox.Parent = KeyUI
-	TextBox.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+	TextBox.BackgroundColor3 = theme["KeyLoginTextBoxColor"]
 	TextBox.BackgroundTransparency = 0.900
 	TextBox.Position = UDim2.new(0.225845605, 0, 0.369597793, 0)
 	TextBox.Size = UDim2.new(0.545224667, 0, 0.142152995, 0)
 	TextBox.Font = Enum.Font.SourceSans
-	TextBox.PlaceholderColor3 = Color3.fromRGB(27, 27, 27)
+	TextBox.PlaceholderColor3 = theme["KeyLoginTextColor"]
 	TextBox.PlaceholderText = "Enter Key"
 	TextBox.Text = ""
-	TextBox.TextColor3 = Color3.fromRGB(0, 0, 0)
+	TextBox.TextColor3 = theme["KeyLoginTextColor"]
 	TextBox.TextScaled = true
 	TextBox.TextSize = 25.000
 	TextBox.TextWrapped = true
@@ -84,13 +97,13 @@ ui.CreateKeySystem = function(key, discord)
 	UITextSizeConstraint.MaxTextSize = 25
 
 	TextButton.Parent = KeyUI
-	TextButton.BackgroundColor3 = Color3.fromRGB(170, 170, 170)
+	TextButton.BackgroundColor3 = theme["TextButtonColor"]
 	TextButton.BorderSizePixel = 0
 	TextButton.Position = UDim2.new(0.258039206, 0, 0.591498554, 0)
 	TextButton.Size = UDim2.new(0.484886885, 0, 0.13504535, 0)
 	TextButton.Font = Enum.Font.FredokaOne
 	TextButton.Text = "Login"
-	TextButton.TextColor3 = Color3.fromRGB(0, 0, 0)
+	TextButton.TextColor3 = theme["TextButtonTextColor"]
 	TextButton.TextScaled = true
 	TextButton.TextSize = 31.000
 	TextButton.TextStrokeColor3 = Color3.fromRGB(255, 255, 255)
@@ -99,23 +112,24 @@ ui.CreateKeySystem = function(key, discord)
 
 	UITextSizeConstraint_2.Parent = TextButton
 	UITextSizeConstraint_2.MaxTextSize = 31
-	
+
 	ts:Create(KeyUI, TweenInfo.new(1, Enum.EasingStyle.Quint), {Size = UDim2.new(0.95, 0, 0.95, 0)}):Play()
-	
+
 	--
-	
+
 	local Input = nil
-	
+
 	TextButton.MouseButton1Click:Connect(function()
 		Input = TextBox.Text
 	end)
-	
+
 	if discord then
 		task.wait(.35)
-		print("Alright")
-		ts:Create(TextBox, TweenInfo.new(.35, Enum.EasingStyle.Quint), {Position = UDim2.new(0.226, 0,0.25, 0)}):Play()
-		task.wait(.1)
-		ts:Create(TextButton, TweenInfo.new(.35, Enum.EasingStyle.Quint), {Position = UDim2.new(0.258, 0,0.45, 0)}):Play()
+		task.delay(.1, function()
+			ts:Create(TextBox, TweenInfo.new(.35, Enum.EasingStyle.Quint), {Position = UDim2.new(0.226, 0,0.178, 0)}):Play()
+			task.wait(.1)
+			ts:Create(TextButton, TweenInfo.new(.35, Enum.EasingStyle.Quint), {Position = UDim2.new(0.258, 0,0.378, 0)}):Play()
+		end)
 		
 		local Discord = Instance.new("Frame")
 		local UICorner = Instance.new("UICorner")
@@ -161,26 +175,58 @@ ui.CreateKeySystem = function(key, discord)
 
 		UITextSizeConstraint_2.Parent = TextButton
 		UITextSizeConstraint_2.MaxTextSize = 29
-		
+
 		TextButton.MouseButton1Click:Connect(function()
 			if setclipboard then
 				setclipboard("https://"..discord)
 			end
 		end)
-		
+
 	end
 	
-	repeat task.wait() until Input
 	
+	if title then
+		task.wait(.35)
+		task.delay(.05, function()
+			ts:Create(TextBox, TweenInfo.new(.35, Enum.EasingStyle.Quint), {Position = UDim2.new(0.226, 0,0.25, 0)}):Play()
+			task.wait(.1)
+			ts:Create(TextButton, TweenInfo.new(.35, Enum.EasingStyle.Quint), {Position = UDim2.new(0.258, 0,0.45, 0)}):Play()
+		end)
+		local TextLabel = Instance.new("TextLabel")
+		local UITextSizeConstraint = Instance.new("UITextSizeConstraint")
+
+		--Properties:
+
+		TextLabel.Parent = KeyUI
+		TextLabel.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+		TextLabel.Position = UDim2.new(0.106617935, 0, 0.0378132015, 0)
+		TextLabel.Size = UDim2.new(0.781864822, 0, 0.157554999, 0)
+		TextLabel.Font = Enum.Font.FredokaOne
+		TextLabel.TextColor3 = Color3.fromRGB(0, 0, 0)
+		TextLabel.TextScaled = true
+		TextLabel.TextSize = 32.000
+		TextLabel.TextWrapped = true
+		TextLabel.Text = title
+
+		UITextSizeConstraint.Parent = TextLabel
+		UITextSizeConstraint.MaxTextSize = 32
+	end
+
+	repeat task.wait() until Input
+
 	ts:Create(KeyUI, TweenInfo.new(.25, Enum.EasingStyle.Quint), {Size = UDim2.new(0, 0, 0.95, 0)}):Play()
 	task.wait(.25)
 	ts:Create(KeyUI, TweenInfo.new(.25, Enum.EasingStyle.Quint), {Size = UDim2.new(0, 0, 0, 0), BackgroundTransparency = 1}):Play()
 	task.wait(.15)
 	ts:Create(Main, TweenInfo.new(.45, Enum.EasingStyle.Quint), {Size = UDim2.new(0.27, 0, 0, 0), BackgroundTransparency = 1}):Play()
 	task.wait(.45)
-
-	return Input:lower():gsub(" ","") == key:lower()
 	
+	pcall(function()
+		KeyLogin:Destroy()
+	end)
+	
+	return Input:lower():gsub(" ","") == key:lower()
+
 end
 
 ui.CreateMain = function()
