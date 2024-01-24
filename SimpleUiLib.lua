3c0022ca63f359f13bfa3c142aeb6cb9527accfc
ROGEEE <106111219+diepedyt@users.noreply.github.com>
2024-01-24 07:14:54 +0000

    Update SimpleUiLib.lua

--- SimpleUiLib.lua
+++ SimpleUiLib.lua
@@ -152,13 +152,9 @@ ui.CreateKeySystem = function(discord, theme, title, keyVar, destroyVar)
 	if discord then
 		local copyLink = discord
 		if type(discord) ~= "string" then
-			discord = discord.displayLink
 			copyLink = discord.realLink
+			discord = discord.displayLink
 		end
-		for i,v in pairs(discord) do
-			warn(i,v)
-		end
-		print(copyLink)
 		task.wait(.35)
 		task.delay(.1, function()
 			ts:Create(TextBox, TweenInfo.new(.35, Enum.EasingStyle.Quint), {Position = UDim2.new(0.226, 0,0.178, 0)}):Play()
